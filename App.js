1795fd54377efbcb30379949e6b93e119d2b858b
Rehan Manzoor <rehan_manzoor@outlook.com>
2018-11-16 18:59:08 +0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import ToastExample from './ToastExample';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+
+      ToastExample.show('Awesome', ToastExample.SHORT);
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
